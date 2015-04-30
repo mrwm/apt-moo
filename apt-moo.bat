15401320a9cc49cf6ab1d46f6edc3caf5fa19993
Will <wiiliam@users.noreply.github.com>
2015-04-29 21:21:44 -0700

    Create apt-moo.bat

---
+++ apt-moo.bat
@@ -0,0 +1,173 @@
+@ECHO OFF
+:1
+color c1
+echo.
+echo         (__) 
+echo         (oo) 
+echo   /------\/ 
+echo  / I    II   
+echo *  /\---/\ 
+echo    ~~   ~~   
+echo ...."Have you mooed today?"...
+timeout /t 1 /nobreak > NUL
+cls
+
+color b2
+echo.
+echo           (__) 
+echo           (oo) 
+echo     /------\/ 
+echo    / I    II   
+echo   *  /\---/\ 
+echo      ~~   ~~   
+echo ...."Have you mooed today?"...
+timeout /t 1 /nobreak > NUL
+cls
+
+color a2
+echo.
+echo             (__) 
+echo             (oo) 
+echo       /------\/ 
+echo      / I    II   
+echo     *  /\---/\ 
+echo        ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 93
+echo.
+echo               (__) 
+echo               (oo) 
+echo         /------\/ 
+echo        / I    II   
+echo       *  /\---/\ 
+echo          ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 84
+echo.
+echo                 (__) 
+echo                 (oo) 
+echo           /------\/ 
+echo          / I    II   
+echo         *  /\---/\ 
+echo            ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 75
+echo.
+echo                   (__) 
+echo                   (oo) 
+echo             /------\/ 
+echo            / I    II   
+echo           *  /\---/\ 
+echo              ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 56
+echo.
+echo                     (__) 
+echo                     (oo) 
+echo               /------\/ 
+echo              / I    II   
+echo             *  /\---/\ 
+echo                ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 67
+echo.
+echo                       (__) 
+echo                       (oo) 
+echo                 /------\/ 
+echo                / I    II   
+echo               *  /\---/\ 
+echo                  ~~   ~~   
+
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+color 48
+echo.
+echo                         (__) 
+echo                         (oo) 
+echo                   /------\/ 
+echo                  / I    II   
+echo                 *  /\---/\ 
+echo                    ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+color 39
+
+echo.
+echo                           (__) 
+echo                           (oo) 
+echo                     /------\/ 
+echo                    / I    II   
+echo                   *  /\---/\ 
+echo                      ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 2a
+echo.
+echo                             (__) 
+echo                             (oo) 
+echo                       /------\/ 
+echo                      / I    II   
+echo                     *  /\---/\ 
+echo                        ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 1b
+echo.
+echo                               (__) 
+echo                               (oo) 
+echo                         /------\/ 
+echo                        / I    II   
+echo                       *  /\---/\ 
+echo                          ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+
+color 0c
+echo.
+echo                                 (__) 
+echo                                 (oo) 
+echo                           /------\/ 
+echo                          / I    II   
+echo                         *  /\---/\ 
+echo                            ~~   ~~   
+echo ...."Have you mooed today?"..."
+timeout /t 1 /nobreak > NUL
+cls
+goto 1
+
+COMMENTS/NOTES:
+To edit the delay time, change the number
+after "/t" in the line 
+"timeout /t 1 /nobreak > NUL"
+
+===
+
+To change the background/text color, change the 
+value after "color". The first value changes the
+background while the second changes the text. 
+(Or is it the other way around?).
+
+Color values: 
+Just run `color` and find out yourself.
